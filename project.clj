800b4ccf3e94936d7bb79303ff641f559522b67d
Alessandro Delgado <adelgado1313@gmail.com>
2017-05-21 00:45:41 +0200

    Make request to api

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
                  [org.clojure/clojurescript "1.9.229"]
                  [reagent "0.6.0"]
                  [re-frame "0.9.2"]
+                 [day8.re-frame/http-fx "0.1.3"]
                  [re-frisk "0.3.2"]
-                 [org.clojure/core.async "0.2.391"]
-                 [re-com "2.0.0"]
                  [secretary "1.2.3"]
                  [garden "1.3.2"]
+                 [cljs-ajax "0.6.0"]
                  [ns-tracker "0.3.0"]]
 
   :plugins [[lein-cljsbuild "1.1.4"]
--- src/cljs/coinwatch/events.cljs
+++ src/cljs/coinwatch/events.cljs
@@ -1,6 +1,8 @@
 (ns coinwatch.events
-    (:require [re-frame.core :as re-frame]
-              [coinwatch.db :as db]))
+  (:require [re-frame.core :as re-frame]
+            [coinwatch.db :as db]
+            [ajax.core :as ajax]
+            [day8.re-frame.http-fx]))
 
 (re-frame/reg-event-db
  :initialize-db
@@ -11,3 +13,34 @@
  :set-active-panel
  (fn [db [_ active-panel]]
    (assoc db :active-panel active-panel)))
+
+
+(re-frame/reg-event-fx        ;; <-- note the `-fx` extension
+ :request
+ (fn                ;; <-- the handler function
+   [{db :db} _]     ;; <-- 1st argument is coeffect, from which we extract db 
+
+   {:http-xhrio {:method          :get
+                 :uri             "http://api.coindesk.com/v1/bpi/currentprice.json"
+                 :format          (ajax/json-request-format)
+                 :response-format (ajax/json-response-format {:keywords? true}) 
+                 :on-success      [:process-response]
+                 :on-failure      [:bad-response]}
+    :db  (assoc db :loading? true)}))
+
+
+(re-frame/reg-event-db
+ :process-response
+ (fn
+   [db [_ response]]           ;; destructure the response from the event vector
+   (-> db
+       (assoc :loading? false) ;; take away that "Loading ..." UI 
+       (assoc :data (js->clj response)))))  ;; fairly lame processing
+
+ (re-frame/reg-event-db
+  :bad-response
+  (fn
+    [db [_ response]]           ;; destructure the response from the event vector
+    (-> db
+        (assoc :loading? false) ;; take away that "Loading ..." UI 
+        (assoc :data (js->clj response)))))  ;; fairly lame processing
