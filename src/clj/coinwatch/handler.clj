092eec3664218b58deb6193e9e5322058fff58fa
Pablo Botelho <pablo.bcb@gmail.com>
2017-05-22 01:11:04 -0300

    improved go loop flow

--- src/clj/coinwatch/handler.clj
+++ src/clj/coinwatch/handler.clj
@@ -1,85 +1,99 @@
 (ns coinwatch.handler
-  "Asynchronous compojure-api application."
   (:require [clojure.core.async :as async]
             [compojure.api.sweet :refer :all]
             [kvlt.core :as kvlt]
             [kvlt.chan :as kchan]
             [ring.util.http-response :refer :all]
-            [manifold.deferred :as d]
             compojure.api.async))
 
 (def currency (atom {}))
 
+(defn ok-status? [req]
+  (= 200 (:status req)))
+
+; investigate other kind of buffers
+(defn get-currency-chan [url]
+  (async/pipe
+   (kvlt.chan/request! {:url url :as :json})
+   (async/chan 1 (filter ok-status?))))
+
+
+;(def d (get-currency "http://api.coindesk.com/v1/bpi/currentprice.json"))
+
+
 (defn update-currency-loop [ms]
+  ; time out conccorrente
+  ; mover logica do fetch corpo pra cima
   (let [url "http://api.coindesk.com/v1/bpi/currentprice.json"
-        interrupt-chan (async/chan)]
+        poison-chan (async/chan)]
     (async/go-loop []
-      (let [price-chan (kvlt.chan/request! {:url url :as :json})
-            [{:keys [body status headers]} selected-chan] (async/alts!
-                                                           [price-chan
-                                                            interrupt-chan])
-            {:keys [code rate]} (-> body (:bpi) (:USD))]
-        (when (identical? selected-chan price-chan)
-          (if (= status 200)
-            (prn
-             (reset! currency
-                     {:displayName (:chartName body)
-                      :symbol code
-                      :dateTime (:date headers)
-                      :price rate})))
+      (let [price-chan (get-currency-chan url)
+            [{:keys [body status headers] :as req} chosen-chan]
+
+            (async/alts!
+                                                                 [price-chan
+                                                                  poison-chan])]
+        (when (not= chosen-chan poison-chan)
+          (if (some? req)
+            (prn (reset! currency
+                         {:displayName (:chartName body)
+                          :symbol (-> body (:bpi) (:USD) (:code))
+                          :dateTime (:date headers)
+                          :price (-> body (:bpi) (:USD) (:rate))})))
           (async/<! (async/timeout ms))
           (recur))))
-    interrupt-chan))
+    poison-chan))
 
-;(def loop-control-chan (update-currency-loop 3000))
+(def loop-control-chan (update-currency-loop 3000))
 
-;(async/close! loop-control-chan)
+(async/close! loop-control-chan)
 
 (def app
   (api
    {:swagger
     {:ui "/"
      :spec "/swagger.json"
-     :data {:info {:title "Simple"
-                   :description "Compojure Api example"}
-            :tags [{:name "api", :description "some apis"}]}}}
+     :data {:info {:title "CoinWatch API Docs"
+                   :description "CoinWatch API Docs"}
+            :tags [{:name "api", :description "foo"}]}}}
 
    (context "/api" []
      :tags ["api"]
 
-     #_(GET "/cancel" []
+     (GET "/cancel" []
        :summary "cancel fetching"
        (do
          (async/close! loop-control-chan)
          (ok {})))
 
      (GET "/price" []
-       ;:return {:displayName String
-       ;         :symbol String
-       ;         :dateTime String
-       ;                                 ;:price Long}
-       ;         :price String}
-                                        ;:query-params [x :- Long, y :- Long]
-       :summary "returns bitcoin price in US dollar"
-       (let [chan (async/chan)]
-           (async/go
-             (try
-               (async/>!
-                chan
-                (->
-                 (ok @currency)
-                 (assoc-in
-                  [:headers "Access-Control-Allow-Origin"]
-                  "*")
-                 (assoc-in
-                  [:headers "Access-Control-Allow-Methods"]
-                  "GET,PUT,POST,DELETE,OPTIONS")
-                 (assoc-in
-                  [:headers "Access-Control-Allow-Headers"]
-                  "X-Requested-With,Content-Type,Cache-Control")))
-               (catch Throwable e
-                 (async/>! chan e))
-               (finally
-                 (async/close! chan))))
-         chan)))))
+       :summary "gets current price in US dollar"
+       :return {:displayName String
+                :symbol String
+                :dateTime String
+                :price String}
+       (async/go
+         (->
+          (ok @currency)
+          (assoc-in
+           [:headers "Access-Control-Allow-Origin"]
+           "*")
+          (assoc-in
+           [:headers "Access-Control-Allow-Methods"]
+           "GET,PUT,POST,DELETE,OPTIONS")
+          (assoc-in
+           [:headers "Access-Control-Allow-Headers"]
+           "X-Requested-With,Content-Type,Cache-Control")))))))
+
+(def c (async/chan 2))
+
+(async/go
+  (loop [i 0]
+    (prn ">" i)
+    (async/>! c i)
+    (recur (inc i))))
+
+(async/go (async/>! c 1))
+
+(async/<!! c)
 
